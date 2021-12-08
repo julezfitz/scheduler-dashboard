97b46559239b7443f21d1bef00cb75f4f4db2460
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-12-08 11:55:41 -0330

    add websocket and onmessage function to update dashboard in real time

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -14,6 +14,8 @@ import {
   getInterviewsPerDay
  } from "helpers/selectors";
 
+import { setInterview } from "helpers/reducers";
+
 const data = [
   {
     id: 1,
@@ -66,12 +68,28 @@ class Dashboard extends Component {
         interviewers: interviewers.data
       });
     });
+
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+    
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
   }
 
   componentDidUpdate(previousProps, previousState) {
     if (previousState.focused !== this.state.focused) {
       localStorage.setItem("focused", JSON.stringify(this.state.focused));
     }
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
   }
 
   selectPanel(id) {
